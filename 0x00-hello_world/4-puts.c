2306c2600bc72feb047d60f4d78d0a0a9bca0bac
Khalil Hc <khalil.hajchaieb123@ail.com>
2023-07-13 05:51:19 -0700

    4-puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/** 
+ * main : outputs 
+ * using 
+ * put
+ *
+ * returns 0 every time
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+}	
