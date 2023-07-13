3d332d35cea9085544ac098c9a12f86095e62db6
Khalil Hc <khalil.hajchaieb123@ail.com>
2023-07-13 08:13:25 -0700

    4 puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-/** 
- * main : outputs 
- * using 
- * put
+/**
+ * main -it out output using put
  *
- * returns 0 every time
+ * Return: always (0)
+ *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
-}	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
