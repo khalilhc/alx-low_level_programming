524b41f4aefa2ffe4cb41000054fe32f3771d566
Khalil Hc <khalil.hajchaieb123@ail.com>
2023-07-18 03:35:01 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - wirte the the lower-case alphabet on a line.
+ *
+ * Return : On success 0.
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
