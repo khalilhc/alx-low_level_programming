3bf6a25c704e727632bfbdde3b0162d334f906c8
Khalil Hc <khalil.hajchaieb123@ail.com>
2023-07-21 04:33:47 -0700

    is upper 0

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+this is the 4th assignment in the C language sprint 
+by the name 0x04.C - more functions and nested loops
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif
